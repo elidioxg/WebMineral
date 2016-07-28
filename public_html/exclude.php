aeefc1244df6f6a01df10503ff3d062a5e6be5e5
elidioxg <elidioxg@gmail.com>
2016-07-28 02:00:18 -0300

    Exclude option updated

--- public_html/exclude.php
+++ public_html/exclude.php
@@ -1,30 +1,30 @@
 <form method='POST' action='exclude_data.php'>
-<select name='minerallist'>
-<?php
-include '../resources/constants.php';
-header('Content-Type: text/html; charset=UTF-8');
-$user = Constants::getUser();
-$local = Constants::getLocal();
-$passwd = Constants::getPasswd();
-$database = Constants::getDatabase();
-$table = Constants::getTableGeneral();
-$name = Constants::getName();
-$connection = mysql_connect($local, $user, $passwd) or print(mysql_error());
-mysql_select_db($database, $connection) or print mysql_error();
-mysql_query("SET NAMES 'utf8'");
-mysql_query("SET character_set_connection=utf8");
-mysql_query("SET character_set_client=utf8");
-mysql_query("SET character_set_results=utf8");
-$sql = "SELECT $name FROM $table ORDER BY $name ASC;";
-$result = mysql_query($sql, $connection) or die(mysql_error());
+    <select name='minerallist'>
+        <?php
+        include '../resources/constants.php';
+        header('Content-Type: text/html; charset=UTF-8');
+        $user = Constants::getUser();
+        $local = Constants::getLocal();
+        $passwd = Constants::getPasswd();
+        $database = Constants::getDatabase();
+        $table = Constants::getTableGeneral();
+        $name = Constants::getName();
+        $connection = mysql_connect($local, $user, $passwd) or die(mysql_error());
+        mysql_select_db($database, $connection) or die(mysql_error());
+        mysql_query("SET NAMES 'utf8'");
+        mysql_query("SET character_set_connection=utf8");
+        mysql_query("SET character_set_client=utf8");
+        mysql_query("SET character_set_results=utf8");
+        $sql = "SELECT $name FROM $table ORDER BY $name ASC;";
+        $result = mysql_query($sql, $connection) or die(mysql_error());
 
-while ($results = mysql_fetch_array($result)) {
-    echo "<option value='".$results[$name]."'>".$results[$name]."</option>";
-}
-mysql_close($connection);
-?>
-  </select>
-  <input type='submit' value='Delete Selected' name='exclude>' >
+        while ($results = mysql_fetch_array($result)) {
+            echo "<option value='" . $results[$name] . "'>" . $results[$name] . "</option>";
+        }
+        mysql_close($connection);
+        ?>
+    </select>
+    <input type='submit' value='Delete Selected' name='exclude>' >
 </form>
 
 
--- public_html/exclude_data.php
+++ public_html/exclude_data.php
@@ -1,4 +1,5 @@
 <?php
+
 include '../resources/constants.php';
 $valueName = filter_input(INPUT_POST, 'minerallist');
 
@@ -7,9 +8,9 @@ $local = Constants::getLocal();
 $passwd = Constants::getPasswd();
 $database = Constants::getDatabase();
 $name = Constants::getName();
-$connection = mysql_connect($local, $user, $passwd) or print (mysql_error());
+$connection = mysql_connect($local, $user, $passwd) or die(mysql_error());
 
-mysql_select_db($database, $connection) or print mysql_error();
+mysql_select_db($database, $connection) or die(mysql_error());
 mysql_query("SET NAMES 'utf8'");
 mysql_query("SET character_set_connection=utf8");
 mysql_query("SET character_set_client=utf8");
@@ -21,17 +22,17 @@ $sqlSelect1 = "SELECT $name FROM $tableGeneral WHERE $name = '$valuename';";
 $result = mysql_query($sqlSelect1, $connection);
 if (mysql_num_rows($result) <= 1) {
     $sqlDelete1 = "DELETE FROM $tableGeneral WHERE $name ='$valueName'";
-    mysql_query($sqlDelete1, $connection) or print(mysql_error());
+    mysql_query($sqlDelete1, $connection) or die(mysql_error());
     $sqlSelect2 = "SELECT $name FROM $tablePhysical WHERE $name = '$valuename';";
     $result = mysql_query($sqlSelect2, $connection);
     $sqlDelete2 = "DELETE FROM $tablePhysical WHERE $name ='$valueName'";
-    mysql_query($sqlDelete2, $connection) or print(mysql_error());
+    mysql_query($sqlDelete2, $connection) or die(mysql_error());
     $sqlSelect3 = "SELECT $name FROM $tableOptical WHERE $name = '$valuename';";
     $result = mysql_query($sqlSelect3, $connection);
     $sqlDelete3 = "DELETE FROM $tableOptical WHERE $name ='$valueName'";
-    mysql_query($sqlDelete3, $connection) or print(mysql_error());
-    echo"<script language='javascript' type='text/javascript'>alert('$fieldName deleted'); window.location.href='./exclude.php'</script>";
+    mysql_query($sqlDelete3, $connection) or die(mysql_error());
+    echo"<script language='javascript' type='text/javascript'>alert('$fieldName deleted'); window.location.href='/WebMineralsData/index.php'</script>";
 } else {
-    echo"<script language='javascript' type='text/javascript'>alert('Not possible to delete $fieldName'); window.location.href='./exclude.php'</script>";
+    echo"<script language='javascript' type='text/javascript'>alert('Not possible to delete $fieldName'); window.location.href='/WebMineralsData/index.php'</script>";
 }
 mysql_close($connection);
